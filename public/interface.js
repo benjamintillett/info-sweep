6d6a6d1b49f41bc11c97b1a46155788a14fecef5
benjamintillett <benjamin.tillett@gmail.com>
2014-09-17 19:35:04 +0100

    midway through tring to make callbacks work on interface.js

--- public/interface.js
+++ public/interface.js
@@ -2,6 +2,22 @@ $(document).ready(function(){
 	$('.userInput').on('submit', function(e){
 		e.preventDefault();
 		user = new User;
-		user.addGitHubUsername($('#username').val());
+		user.addGitHubUsername($('#username').val(),function(user){
+			console.log(user.showFollowers()) 
+			var template = $('#userSection').html();
+			$('#sampleArea').append(Mustache.render(template,user));
+		});
+			
+		
+		
+		
 	})
-})
+});
+
+
+
+
+// var person = { followers: user.followers,
+// 				login: user.gitHubUsername.toString(),
+// 				repos: user.repos,
+// 				avatar: user.avatar};
--- public/user.js
+++ public/user.js
@@ -1,7 +1,8 @@
 var GITHUB_TOKEN = "1860e17c99b5696fe8d9d39f63ddd180d44284ff"
 
 function User(name) {
-	this.name = name 
+	this.name = name;
+	this.followers = "56";
 }
 
 User.prototype.addGitHubUsername = function(username) {
@@ -16,17 +17,16 @@ User.prototype.addGitHubUsername = function(username) {
 
 User.prototype.getGitHubData = function(callback) {
 	$.get(this.gitHubUrl, function(user){
-		 callback.followers(user.followers);
-		 callback.login(user.login);
-		 callback.repos(user.public_repos)
-		 callback.avatar(user.avatar_url)	
+		 callback.setFollowers(user.followers);
+		 callback.repos(user.public_repos); 
+		 callback.avatar(user.avatar_url);
 	}).fail(function(){
-            alert('Could not find this user.');
-          }).always(function() {
-            $('#username').val('');
-          })
+         alert('Could not find this user.');
+    }).always(function() {
+         $('#username').val('');
+    })
 }
-User.prototype.followers = function(followers) {
+User.prototype.setFollowers = function(followers) {
 	this.followers = followers;
 }
 User.prototype.repos = function(repos) {
@@ -39,3 +39,6 @@ User.prototype.avatar = function(avatar) {
 User.prototype.login = function(login) {
 	this.login = login;
 }
+User.prototype.showFollowers = function(first_argument) {
+	return this.followers
+};
